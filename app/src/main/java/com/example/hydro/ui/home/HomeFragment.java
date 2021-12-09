37ef0b0b719c2a76cbca00b71ba33ee507c545b1
daempasha <42499726+daempasha@users.noreply.github.com>
2021-12-09 22:44:26 +0000

    Location is now working and also added openweathermap API

--- app/src/main/java/com/example/hydro/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/hydro/ui/home/HomeFragment.java
@@ -1,24 +1,123 @@
 package com.example.hydro.ui.home;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.hydro.R;
 import com.example.hydro.databinding.FragmentHomeBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationRequest locationRequest;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        updateGPS();
+    }
+
+    private void getWeatherForLocation(double latitude, double longitude) {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+        String url = "https://api.openweathermap.org/data/2.5/weather?lat="+latitude + "&lon=" + longitude + "&appid={API key}";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //textView.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    private void updateGPS() {
+        // Get permission from user
+        //Get current location
+        //Update
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            //Granted permission
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    //Got permission
+                    double latitude, longitude;
+
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+
+                    getWeatherForLocation(latitude, longitude);
+                }
+            });
+        } else {
+            //No permission
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+        }
+    }
+
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    updateGPS();
+                } else {
+                    Toast.makeText(getActivity(), "This application requires the location permission to be granted in order to function.", Toast.LENGTH_SHORT).show();
+                }
+            });
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -35,8 +134,10 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
--- app/src/main/java/com/example/hydro/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/hydro/ui/home/HomeViewModel.java
@@ -1,17 +1,30 @@
 package com.example.hydro.ui.home;
 
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
-
+    private double[] mCoordinates;
     public HomeViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
+
+        mCoordinates = new double[]{0.0f, 0.0f};
     }
+
+
+    public void setCoordinates(double latitude, double longitude){
+        mCoordinates = new double[]{latitude, longitude};
+    }
+
 
     public LiveData<String> getText() {
         return mText;
