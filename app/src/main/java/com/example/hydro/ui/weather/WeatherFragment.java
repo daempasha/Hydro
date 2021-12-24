6a7a52e04a5b5b98a0083ef24266697d94076929
daempasha <42499726+daempasha@users.noreply.github.com>
2021-12-24 23:41:09 +0000

    Added API key file

--- app/src/main/java/com/example/hydro/ui/weather/WeatherFragment.java
+++ app/src/main/java/com/example/hydro/ui/weather/WeatherFragment.java
@@ -1,28 +1,186 @@
 package com.example.hydro.ui.weather;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
-import com.example.hydro.databinding.FragmentTodoBinding;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.hydro.R;
 import com.example.hydro.databinding.FragmentWeatherBinding;
+import com.example.hydro.models.Weather;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 
 public class WeatherFragment extends Fragment {
 
     private FragmentWeatherBinding binding;
 
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationRequest locationRequest;
+
+    private TextView cityText;
+    private TextView temperatureText;
+    private TextView weatherDescriptionText;
+    private ImageView weatherIconText;
+    private TextView windSpeedText;
+    private TextView humidityText;
+    private TextView pressureText;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        updateGPS();
+    }
+
+    private void getWeatherForLocation(double latitude, double longitude) {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+        String url = "https://api.openweathermap.org/data/2.5/weather?lat="+latitude + "&lon=" + longitude + "&appid=" + getResources().getString(R.string.openweathermap_api_key);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Weather weather = new Weather(response);
+                        updateWeather(weather.city, weather.temperature, weather.description, weather.iconUrl, weather.windSpeed, weather.humidity, weather.pressure);
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //textView.setText("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+
+    private void updateWeather(String cityValue, String temperatureValue, String descriptionValue, String iconUrl, String windSpeedValue, String humidityValue, String pressureValue){
+        Log.e("TEST", "cityValue is:" + cityValue);
+        Log.e("TEST", "tempValue is:" + temperatureValue);
+        Log.e("TEST", descriptionValue);
+        Log.e("TEST", iconUrl);
+        Log.e("TEST", windSpeedValue);
+        Log.e("TEST", humidityValue);
+        Log.e("TEST", pressureValue);
+
+        cityText.setText(cityValue);
+        temperatureText.setText(temperatureValue);
+        weatherDescriptionText.setText(descriptionValue);
+        windSpeedText.setText(windSpeedValue);
+        humidityText.setText(humidityValue);
+        pressureText.setText(pressureValue);
+        Picasso.get().load(iconUrl).into(weatherIconText);
+
+    }
+
+
+    private void updateGPS() {
+        // Get permission from user
+        //Get current location
+        //Update
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            //Granted permission
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    //Got permission
+                    if(location != null){
+
+                        double latitude, longitude;
+
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
+
+                        getWeatherForLocation(latitude, longitude);
+                    } else {
+                        Toast.makeText(getActivity(), R.string.noLocationError, Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            });
+        } else {
+            //No permission
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+        }
+    }
+
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    updateGPS();
+                } else {
+                    Toast.makeText(getActivity(), "This application requires the location permission to be granted in order to function.", Toast.LENGTH_SHORT).show();
+                }
+            });
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
 
         binding = FragmentWeatherBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        //Get views
+        this.cityText = binding.city;
+        this.temperatureText = binding.temperature;
+        this.weatherDescriptionText = binding.description;
+        this.weatherIconText = binding.weatherIcon;
+        this.windSpeedText = binding.windSpeed;
+        this.humidityText = binding.humidity;
+        this.pressureText = binding.pressure;
+
+
+/*        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });*/
+
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
