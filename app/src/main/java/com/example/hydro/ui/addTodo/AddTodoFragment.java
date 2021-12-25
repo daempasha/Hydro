8b3db619febe77f1081f5b155b715bbb53702ad5
daempasha <42499726+daempasha@users.noreply.github.com>
2021-12-25 20:32:00 +0000

    Added date picker and any dates before today are blocked

--- app/src/main/java/com/example/hydro/ui/addTodo/AddTodoFragment.java
+++ app/src/main/java/com/example/hydro/ui/addTodo/AddTodoFragment.java
@@ -1,7 +1,11 @@
 package com.example.hydro.ui.addTodo;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -9,24 +13,76 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.hydro.R;
+import com.google.android.material.datepicker.CalendarConstraints;
+import com.google.android.material.datepicker.DateValidatorPointForward;
+import com.google.android.material.datepicker.MaterialDatePicker;
 
 
-public class AddTodoFragment extends Fragment {
+public class AddTodoFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
 
     private TextView descriptionText;
-    private TextView dateText;
-    private Button dateButton;
+    private TextView dateView;
+    private Button submitButton;
+    private MaterialDatePicker datePicker;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_todo, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        descriptionText = (TextView) view.findViewById(R.id.todoDescription);
+        dateView = (TextView) view.findViewById(R.id.todoDateView);
+        submitButton = (Button) view.findViewById(R.id.addTodoItemButton);
+
+        CalendarConstraints.Builder calendarConstraintsBuilder = new CalendarConstraints.Builder().setValidator(DateValidatorPointForward.now());
+        CalendarConstraints calendarConstraints = calendarConstraintsBuilder.build();
+
+        datePicker =  MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select date")
+                .setCalendarConstraints(calendarConstraints)
+                .build();
+
+
+        dateView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    showDatePicker();
+
+                }
+            }
+        });
+
+        dateView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePicker();
+
+            }
+        });
+    }
+
+    private void showDatePicker() {
+        if(!datePicker.isVisible()){
+            datePicker.show(getParentFragmentManager(), "tag");
+
+        }
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
 
     }
 }
